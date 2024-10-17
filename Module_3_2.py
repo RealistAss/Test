fcb17d31c0e90aa8697edffb0a3c1ac8b4f09db1
Илья <Ilyamolot@mail.ru>
2024-10-17 17:09:59 +0300

    initial alo

---
+++ Module_3_2.py
@@ -0,0 +1,19 @@
+def send_mail(message, recipient, sender = 'university.help@gmail.com'):
+
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif '@' not in  recipient or '@' not in sender:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    elif recipient.endswith ('.com' or '.ru' or '.net'):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+
+    elif sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+
+send_mail('Alpen gold', 'university.help@gmail.com', )
+send_mail('Milka', 'Ilyamolot.mail.ru')
+send_mail('Dove', 'Ilyamolot@mail.ru')
+send_mail('Crispy', 'Ilyamolot@mail.ru', 'yamolot@mail.ru' )
