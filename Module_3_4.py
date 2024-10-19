09d4b32b9b8171f72aeed1eb60524750434c8c61
Илья <Ilyamolot@mail.ru>
2024-10-19 15:09:09 +0300

    initial

--- Module_3_4.py
+++ Module_3_4.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for x in other_words:
-        if x in root_word :
+        if x in root_word:
             same_words.append(x)
             print(same_words)
 single_root_words('leonardo', 'leo', 'nard', 'awdf' )
