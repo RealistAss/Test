798191c934e292d7766df53cf0f835c8878e9a86
Илья <Ilyamolot@mail.ru>
2024-10-19 14:51:33 +0300

    initial alo

--- Module_3_1.py
+++ Module_3_1.py
@@ -1,19 +1,24 @@
 calls = 0
 def count_calls():
-    print(calls)
+    global calls
+    calls += 1
+
 def string_info(string):
-        print(string.upper())
-        print(string.lower())
+    count_calls()
+    return(string.lower(), string.upper(), len(string))
+
 def is_contains(string, list_to_search):
-             a = [x.lower() for x in list_to_search]
-             if string.lower() in a:
-                print(True)
-             else:
-                print(False)
-                print(string, a)
+       count_calls()
+       return string.lower() in (x.lower() for x in list_to_search)
+
+
 
 print(string_info('Capybara'))
+
 print(string_info('Armageddon'))
+
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+
 print(is_contains('cycle', ['recycling', 'cyclic']))
+
 print(calls)
